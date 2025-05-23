eef2e074c55220025a2f9043e964bf115ab47481
Ahmed Hegab <139879264+AHegab@users.noreply.github.com>
2025-05-23 23:15:10 +0300

    Update App.js

--- my-bootstrap-app2/src/App.js
+++ my-bootstrap-app2/src/App.js
@@ -6,14 +6,39 @@ import TaskDetail from './pages/TaskDetails';
 import Tasks from './pages/Tasks';
 import { useEffect } from 'react';
 import Cookies from 'js-cookie';
-import Callback from './pages/Callback'; // You'll need to create this
-import LoginErrorPage from './pages/LoginErrorPage'; // You'll need to create this
+import Callback from './pages/Callback'; // your existing Callback
+import LoginErrorPage from './pages/LoginErrorPage';
+
 function App() {
   useEffect(() => {
-    // Check for existing session on app load
-    const token = Cookies.get('idToken');
-    if (token) {
-      // You might want to verify token or check expiration here
+    // if we got redirected here with tokens in the URL, persist them
+    const params = new URLSearchParams(window.location.search);
+    const idToken = params.get('id_token');
+    const accessToken = params.get('access_token');
+    const refreshToken = params.get('refresh_token');
+
+    if (idToken && accessToken) {
+      // set your cookies exactly as you would in Callback
+      Cookies.set('idToken', idToken, {
+        expires: 1,
+        secure: false,      // set to true in prod HTTPS
+        sameSite: 'lax'
+      });
+      Cookies.set('accessToken', accessToken, {
+        expires: 1,
+        secure: false,
+        sameSite: 'lax'
+      });
+      if (refreshToken) {
+        Cookies.set('refreshToken', refreshToken, {
+          expires: 30,
+          secure: false,
+          sameSite: 'lax'
+        });
+      }
+
+      // remove the tokens from the URL
+      window.history.replaceState({}, '', window.location.pathname);
     }
   }, []);
 
@@ -28,9 +53,6 @@ function App() {
           <Route path="/create-task" element={<CreateTask />} />
           <Route path="/callback" element={<Callback />} />
           <Route path="/login" element={<LoginErrorPage />} />
-          {/* <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/profile" element={<Profile />} /> */}
         </Routes>
       </div>
     </Router>
